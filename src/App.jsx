d0c6ba23b68cade470e842fa7494a3f0c310b434
ChauhanVinay <vc0627344@gmail.com>
2026-05-17 11:58:00 +0530

    Initial commit with components

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import ExpenseItem from './components/ExpenseItem'
+import Footer from './Footer'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <section id="center">
+        <div className="hero">
+          <img src={heroImg} className="base" width="170" height="179" alt="" />
+          <img src={reactLogo} className="framework" alt="React logo" />
+          <img src={viteLogo} className="vite" alt="Vite logo" />
+        </div>
+        <div>
+          <h1>Let's Get started</h1>
+        </div>
+       
+        <ExpenseItem />
+
+        <Footer />
+  
+      </section>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/ExpenseItem.jsx
@@ -0,0 +1,17 @@
+function ExpenseItem() {
+  return (
+    <>
+      <div>15 August 2023</div>
+      <div>
+        <h2>Book</h2>
+        <h2>Location</h2>
+        <div>Delhi</div>
+        <h2>Amount</h2>
+        <div>10$</div>
+      </div>
+
+    </>
+  )
+}
+
+export default ExpenseItem;
