8f61be19bf99a2aa0794b41e0191aa0e7b830d4b
ChauhanVinay <vc0627344@gmail.com>
2026-05-18 14:07:37 +0530

    Added a new CSS style file

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,16 +1,16 @@
+import "./ExpenseItem.css";
+
 function ExpenseItem() {
   return (
-    <>
+    <div className="expense-item">
       <div>15 August 2023</div>
-      <div>
+      <div className="expense-item__description">
         <h2>Book</h2>
-        <h2>Location</h2>
-        <div>Delhi</div>
-        <h2>Amount</h2>
-        <div>10$</div>
+ <div className="expense-item__location">Delhi</div>
+   <div className="expense-item__price">10$</div>
       </div>
 
-    </>
+    </div>
   )
 }
 
