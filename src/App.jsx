d41a231fdcbc227219177ceafccd9fcfc86f6748
ChauhanVinay <vc0627344@gmail.com>
2026-05-18 15:17:29 +0530

    Adding props for passing value to child component

--- src/App.jsx
+++ src/App.jsx
@@ -13,18 +13,40 @@ function App() {
     <>
       <section id="center">
         <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
+<img src={heroImg} className="base" width="170" height="179" alt="Hero"/>
+<img src={reactLogo} className="framework" alt="React logo" />
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
           <h1>Let's Get started</h1>
         </div>
        
-        <ExpenseItem />
+        <ExpenseItem 
+          date={new Date(2023, 7, 15)}
+          location="Bangalore"
+          title="Insurance"
+          price={50}
+        />
+        <ExpenseItem 
+          date={new Date(2026, 3, 25)}
+          location="Delhi"
+          title = "Book"
+          price="20"
+        />
+        <ExpenseItem
+         date={new Date(2026, 5, 10)}
+          location="Hyderabad"
+          title = "Food"
+          price="30"
+         />
+        <ExpenseItem 
+          date={new Date(2026, 8, 5)}
+          location = "Mumbai"
+          title = "Laptop"
+          price="500"
+        />
 
         <Footer />
-  
       </section>
     </>
   )
--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,19 +1,18 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
-  const expenseDate = new Date(2023, 7, 15).toISOString();
-  const expenseTitle = "Insurance";
-  const expanseAmount = 50;
-  const expenseLocation = "Bangalore";
+function ExpenseItem(props) {
+  // const expenseDate = new Date(2023, 7, 15).toISOString();
+  // const expenseTitle = "Insurance";
+  // const expanseAmount = 50;
+  // const expenseLocation = "Bangalore";
   return (
     <div className="expense-item">
-      <div>{expenseDate}</div>
-        <div className="expense-item__location">{expenseLocation}</div>
+      <div>{props.date.toISOString()}</div>
+        <div className="expense-item__location">{props.location}</div>
         <div className="expense-item__description">
-          <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">{expanseAmount}$</div>
+          <h2>{props.title}</h2>
+        <div className="expense-item__price">{props.price}$</div>
       </div>
-
     </div>
   )
 }
