aa51307b2c74997e9e138230270709536f45e156
ChauhanVinay <vc0627344@gmail.com>
2026-05-20 18:04:27 +0530

    Managing multiple state

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,17 @@
-
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 // import ExpenseItem from './components/ExpenseItem'
-import Expenses from './components/Expenses/Expenses';
-import Footer from './Footer'
-import './App.css'
+import "./App.css";
+import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
+import Footer from "./Footer";
 
 function App() {
-  
-
   return (
     <>
       <section id="center">
-<div className="hero">
-<img src={heroImg} className="base" width="170" height="179" alt="Hero"/>
-<img src={reactLogo} className="framework" alt="React logo" />
-<img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
         <div>
-        <h1>Let's Get started</h1>
-        <Expenses></Expenses>
+          <h1>Let's Get started</h1>
+          <NewExpense />
+          <Expenses></Expenses>
         </div>
 
         <Footer />
@@ -29,4 +20,4 @@ function App() {
   );
 }
 
-export default App
+export default App;
